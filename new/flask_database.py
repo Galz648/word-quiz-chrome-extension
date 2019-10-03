dd2895f1c8674953b0a9a4678a4768d584e06a85
rrviens <39856416+rrviens@users.noreply.github.com>
2019-10-03 21:21:11 +0300

    comment changes

--- new/flask_database.py
+++ new/flask_database.py
@@ -1,30 +1,22 @@
-# database file
-# datetime
-from datetime import datetime
-# db-ORM import 
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime                               # database file, datetime
+from flask_sqlalchemy import SQLAlchemy                     # db-ORM import 
 from main_flask import app
 
-# database setup
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# using sqlite db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
-# user database-table setup
-class User(db.Model):
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False        # database setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # using sqlite db
+db = SQLAlchemy(app)                                        # define database for the app
+
+class User(db.Model):                                       # user database-table setup
     pass
-# user-selection database-table setup
-class UserSelection(db.Model):
-    # id, word, occurence, datastamp, username - not added yet 
-    id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(60), unique=True)    
+
+class UserSelection(db.Model):                              # user-selection database-table setup
+    id = db.Column(db.Integer, primary_key=True)            # id, word, occurence, datastamp, username - not added yet
+    word = db.Column(db.String(60), unique=True)            
     occurence = db.Column(db.Integer)
     datestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
-    def __repr__(self):
+    def __repr__(self):                                     # representation function
         return f'id:{self.id}|word:{self.word}|occcurence:{self.occurence}|datestamp:{self.datestamp}'
 
 
-    def insert_word(self, parameter_list):
-        pass
--- new/main_flask.py
+++ new/main_flask.py
@@ -1,21 +1,16 @@
-# flask web server
-# flask imports
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from os import system
-# logging import
-import logging
+from flask import Flask, jsonify, request   # flask web server
+from flask_cors import CORS                 # flask imports
+from os import system                       
+import logging                              # logging import
 
-# delete so the chrome extension will load
-system('rm -rf __pycache__')
+system('rm -rf __pycache__')                # delete so the chrome extension will load
 
-# Flask app setup
-app = Flask(__name__)
-# Cross-origin resource sharing
-CORS(app)
+app = Flask(__name__)                       # Flask app setup
 
-# Create and configure logger
-LOG_FILE = 'chrome_extension.log'
+CORS(app)                                   # Cross-origin resource sharing
+
+
+LOG_FILE = 'chrome_extension.log'           # Create and configure logger
 LOG_FORMAT = '%(asctime)s:%(levelname)s:%(name)s -> %(message)s'
 logging.basicConfig(
             filename = LOG_FILE,
@@ -30,21 +25,19 @@ logger = logging.getLogger(__name__)
 #stream_handler.setFormatter(LOG_FORMAT)
 #stream_handler.setLevel(logging.INFO)
 
-@app.route('/arguments', methods=['GET'])
+@app.route('/arguments', methods=['GET'])   # gets arguments from the arguments list
 def test_route():
    return jsonify({'message': 'true'})
 
-@app.route('/api', methods=['POST'])
+@app.route('/api', methods=['POST'])        # posts to api to look up word meaning 
 def post_word():
-    """
-    POST word 
-    example: GET /api?word=dog
     
-    return : json object that contains ---
-    """
-    logger.info(f' user-selection : {request.json["sel"]}')
-    
-    return jsonify({'message':'success'})
+# POST word 
+# example: GET /api?word=dog
+# return : json object that contains ---
+                                    
+    logger.info(f' user-selection : {request.json["sel"]}') #
+    return jsonify({'message':'success'})   # returns json msg w/ 'success'
 
 if __name__ == '__main__':
     app.run(port=5000)
